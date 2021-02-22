ef678b70c061b2e68d3a488c1e2126ea2252dbf0
Andrea Rogoff <andrea.rogoff@gmail.com>
2021-02-22 15:40:39 -0800

    Exercise files from Launch School's Introduction to Ruby.

---
+++ the_basics/Exercise3.rb
@@ -0,0 +1,10 @@
+movies = { halloween: 1995, a_river_runs_through_it: 1990, picture_perfect: 2004 }
+puts movies[:halloween]
+puts movies[:a_river_runs_through_it] 
+puts movies[:picture_perfect]
+
+{:halloween => 1995, :a_river_runs_through_it => 1990, :picture_perfect => 2004 }
+
+puts movies[:halloween]
+puts movies[:a_river_runs_through_it] 
+puts movies[:picture_perfect]
---
+++ the_basics/Exercise4.rb
@@ -0,0 +1,5 @@
+dates = [1995, 1990, 2004]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
